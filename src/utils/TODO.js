08d5ffc65b6718adc795e06218499007c6aa84f5
Joynal Dsouza <joynaldsouza2@gmail.com>
2024-02-08 23:54:19 +0530

    update TODO

--- src/utils/TODO.js
+++ src/utils/TODO.js
@@ -1,5 +1,8 @@
-// TODO :  format to currency
 // TODO : add a pie chart for expenses by type
 // TODO : edit budget and expenses
 // TODO : delete types
-// TODO : add Notes to expenses
+// TODO : Widget for expenses
+
+// DONE
+//   add Notes to expenses
+//  format to currency
