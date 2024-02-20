fd5f0ff76f60858f3f89416ab66eea64e1d3b21f
Joynal Dsouza <joynaldsouza2@gmail.com>
2024-02-20 22:57:03 +0530

    auto close dropdown

--- App.js
+++ App.js
@@ -1,34 +1,14 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-} from 'react-native';
+import {SafeAreaView, StyleSheet} from 'react-native';
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
 import Navigation from './src/Navigation/navigation';
 
-import {GoalsItem, Plan, Profile} from './src/realm/models';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import LoginScreen from './src/screens/LoginScreen';
-import HomeScreen from './src/screens/HomeScreen';
 import schemas from './src/realm/models/schemas';
 import {RealmProvider} from '@realm/react';
 import {BSON} from 'realm';
 import {ExpenseType} from './src/realm/models/User';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
-const Stack = createNativeStackNavigator();
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
     <RealmProvider
       schema={schemas}
